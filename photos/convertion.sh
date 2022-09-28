6dfea149f2aa1043157d749b298998a2c0276c65
michbur <michalburdukiewicz@gmail.com>
2022-09-28 18:52:28 +0200

    added photos

---
+++ photos/convertion.sh
@@ -0,0 +1,1 @@
+mogrify -path photos/photos-small -filter Triangle -define filter:support=2 -thumbnail 300 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB photos/Captura.PNG
